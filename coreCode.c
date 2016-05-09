07067fbb8991d50cf1a24e885d43c77308bbc2e3
dan71853 <sommerdani@myvuw.ac.nz>
2016-05-09 20:32:00 +1200

    Update coreCode.c
    Added a comment

--- coreCode.c
+++ coreCode.c
@@ -89,7 +89,7 @@ int main()
 	int sum = 0;
 	int i, w, s;
 	for (i=0, i<320, i++){
-		w = i*get_pixel(i, 120, 3);
+		w = i*get_pixel(i, 120, 3);      //Should not be * by i at this point.
 		if (w > 127){s = 1;}
 		else {s = 0;}
 		sum = sum + (i-160)*s;
